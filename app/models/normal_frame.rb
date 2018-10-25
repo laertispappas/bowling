21281060ac828cba0944b76512fe9a79042f0959
Laerti Papa <laerti.papa@xing.com>
2018-10-25 21:03:20 +0200

    Removes redundant if statement in NormalFrame#strike_bonus

--- app/models/normal_frame.rb
+++ app/models/normal_frame.rb
@@ -13,12 +13,12 @@ class NormalFrame < Frame
   end
 
   def strike_bonus
+    # next is strike or double strike
     if next_frame.strike? && !next_frame.is_a?(LastFrame)
       next_frame.rolls[0].score + next_frame.next_frame.rolls[0]&.score.to_i
-    elsif next_frame.strike? && next_frame.is_a?(LastFrame)
-      next_frame.rolls[0].score + next_frame.rolls[1]&.score.to_i
     else
-      next_frame.rolls.sum(&:score)
+      # next frame may not have any rolls yet
+      next_frame.rolls[0]&.score.to_i + next_frame.rolls[1]&.score.to_i
     end
   end
 
